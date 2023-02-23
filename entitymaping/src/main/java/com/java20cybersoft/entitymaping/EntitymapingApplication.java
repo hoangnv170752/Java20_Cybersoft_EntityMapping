a1403c7324bd8c78ec27a719c9ffc708df113ed5
Hoang Nguyen Van <akashi@Hoangs-MacBook-Pro.local>
2023-02-23 22:19:59 +0700

    Entity Mapping

---
+++ entitymaping/src/main/java/com/java20cybersoft/entitymaping/EntitymapingApplication.java
@@ -0,0 +1,13 @@
+package com.java20cybersoft.entitymaping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitymapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntitymapingApplication.class, args);
+	}
+
+}
---
+++ entitymaping/src/test/java/com/java20cybersoft/entitymaping/EntitymapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java20cybersoft.entitymaping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntitymapingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
